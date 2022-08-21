3e947d62e7db67c90262d395b731ced14888a858
Hugo Saldarriaga <hugosmm2000@gmail.com>
2022-08-20 19:09:42 -0500

    Fix a bug with config

--- plugin/src/main/java/io/github/emanuelmcp/inventory/MainInventory.java
+++ plugin/src/main/java/io/github/emanuelmcp/inventory/MainInventory.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainInventory implements InventoryCreator{
+public class MainInventory implements InventoryCreator {
    
    private final RpMmo plugin;
    
@@ -25,27 +25,27 @@ public class MainInventory implements InventoryCreator{
    
    @Override
    public @NotNull Inventory createInventory() {
-   
+      
       FileConfiguration config = plugin.getConfig();
       
       String title = config.getString(ChatUtils.color("menu.title"));
       
       Inventory mainInventory = Bukkit.createInventory(null, 9, title);
       
-      for(String key: config.getConfigurationSection("menu.skills").getKeys(false)) {
+      for(String key : config.getConfigurationSection("menu.skills").getKeys(false)) {
          
-         ConfigurationSection section = config.getConfigurationSection("skills." + key + ".");
+         ConfigurationSection section = config.getConfigurationSection(key + ".");
          String name = section.getString("name-item");
          List<String> lore = section.getStringList("lore");
          int position = section.getInt("position");
          String icon = section.getString("icon");
-   
+         
          ItemStack item = new ItemStack(Material.valueOf(icon));
          ItemMeta meta = item.getItemMeta();
          meta.setDisplayName(ChatUtils.color(name));
          meta.setLore(ChatUtils.colorList(lore));
          item.setItemMeta(meta);
-   
+         
          mainInventory.setItem(position, item);
       }
       
